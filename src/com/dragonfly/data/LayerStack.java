28eba42f2c4af44f200ce77712b75a86cb17163c
Matty Thorne <matthew.thorne@blueshifttech.co.uk>
2017-04-21 15:05:43 +0100

    Development of layer stack and swarm algorithm

--- src/com/dragonfly/data/LayerStack.java
+++ src/com/dragonfly/data/LayerStack.java
@@ -3,13 +3,41 @@ package com.dragonfly.data;
 import java.util.ArrayList;
 
 public class LayerStack {
+	
 	public ArrayList<DataSet> dataSets = new ArrayList<DataSet>();
+	private int fieldWidth;
+	private int fieldHeight;
 
 	public LayerStack(ArrayList<DataSet> dataSets) {
 		super();
 		this.dataSets = dataSets;
+		getFieldDimensions();
 	}
 	
+	public int getFieldWidth() {
+		return fieldWidth;
+	}
+
+	public int getFieldHeight() {
+		return fieldHeight;
+	}
+	
+	private void getFieldDimensions() {
+		
+		this.fieldWidth = Integer.MAX_VALUE;
+		this.fieldHeight = Integer.MAX_VALUE;
+		
+		for(DataSet dataLayer:this.dataSets) {
+			int x = dataLayer.getxSize();
+			int y = dataLayer.getySize();
+			if(x < fieldWidth) this.fieldWidth = x;
+			if(y < fieldHeight) this.fieldHeight = y;
+		}
+
+	}
+
+	
+	// Needs improvement
 	public Double getWeightedValue(int x, int y){
 		
 		Double weightedValue=0.0;
--- src/com/dragonfly/swarm/Engine.java
+++ src/com/dragonfly/swarm/Engine.java
@@ -2,13 +2,29 @@ package com.dragonfly.swarm;
 
 import com.dragonfly.data.LayerStack;
 
+/**
+ * 1. Randomly initialise a swarm of insects at random positions in the search space
+ * 2. evaluate all positions and update the global best position and personal best positions
+ * 3. update each velocity based on the relative position of the global best position, 
+ *    the current velocity of the insect, the personal best position of the insect and
+ *    a random vector
+ * 4. rinse and repeat
+ * 
+ * @author matt
+ *
+ */
 public class Engine {
 	private LayerStack layerStack;
 	private int swarmSize;
+	private int fieldWidth;
+	private int fieldHeight;
 
-	public Engine(LayerStack layerStack) {
+	public Engine(LayerStack layerStack, int swarmSize) {
 		super();
 		this.layerStack = layerStack;
+		this.swarmSize = swarmSize;
+		this.fieldHeight = layerStack.getFieldHeight();
+		this.fieldWidth = layerStack.getFieldWidth();
 	}
 	
 	
